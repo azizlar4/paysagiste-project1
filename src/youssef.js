73e393a336ce943dcfb8ee2264690f4dbab77f56
azizlar4 <laroussiaziz4@gmail.com>
2023-03-01 18:54:13 +0100

    first commit

--- src/youssef.js
+++ src/youssef.js
@@ -1,40 +1,37 @@
-import Cardd from "components/cards/ThreeColSlider";
-import Headerr from "components/headers/light";
-import AboutUs from "pages/AboutUs";
-import Features from "components/features/TwoColWithTwoFeaturesAndButtons";
-import Featuress from "components/features/ThreeColCenteredStatsPrimaryBackground";
-import Featuresss from "components/features/TwoColSingleFeatureWithStats2";
-import Featuressss from "components/features/ThreeColWithSideImageWithPrimaryBackground";
-import Hero from "components/hero/BackgroundAsImageWithCenteredContent.js";
-import Testimonial from "components/testimonials/SimplePrimaryBackground.js";
-import TabGrid from "components/cards/TabCardGrid.js";
-import FAQ from "components/faqs/SimpleWithSideImage.js";
-// import SubscribeNewsLetterForm from "components/forms/SimpleSubscribeNewsletter.js";
-import ContactUsForm from "components/forms/TwoColContactUsWithIllustrationFullForm.js";
-import Footer from "components/footers/MiniCenteredFooter.js";
+// import Cardd from "components/cards/ThreeColSlider";
+// import Headerr from "components/headers/light";
+// import AboutUs from "pages/AboutUs";
+// import Features from "components/features/TwoColWithTwoFeaturesAndButtons";
+// import Featuress from "components/features/ThreeColCenteredStatsPrimaryBackground";
+// import Featuresss from "components/features/TwoColSingleFeatureWithStats2";
+// import Featuressss from "components/features/ThreeColWithSideImageWithPrimaryBackground";
+// import Hero from "components/hero/BackgroundAsImageWithCenteredContent.js";
+// import Testimonial from "components/testimonials/SimplePrimaryBackground.js";
+// import TabGrid from "components/cards/TabCardGrid.js";
+// import FAQ from "components/faqs/SimpleWithSideImage.js";
+// // import SubscribeNewsLetterForm from "components/forms/SimpleSubscribeNewsletter.js";
+// import ContactUsForm from "components/forms/TwoColContactUsWithIllustrationFullForm.js";
+// import Footer from "components/footers/MiniCenteredFooter.js";
 
-
-
-export default function Youssef() {
-  return (
-    <div >
-{/* <Cardd /> */}
-{/* <Headerr />
-hello guys laldg,qkndvqnkvkv,lc,vcv;mx;vmxv; */}
-<Hero />
-<Features />
-<Featuressss />
-<Featuresss />
-<Featuress />
-<Cardd />
-<TabGrid />
-<Testimonial />
-<FAQ />
-<ContactUsForm />
-<Footer />
-{/* <Footer /> */}
-{/* <AboutUs /> */}
-    </div>
-  );
-}
-
+// export default function Youssef() {
+//   return (
+//     <div >
+// {/* <Cardd /> */}
+// {/* <Headerr />
+// hello guys laldg,qkndvqnkvkv,lc,vcv;mx;vmxv; */}
+// <Hero />
+// <Features />
+// <Featuressss />
+// <Featuresss />
+// <Featuress />
+// <Cardd />
+// <TabGrid />
+// <Testimonial />
+// <FAQ />
+// <ContactUsForm />
+// <Footer />
+// {/* <Footer /> */}
+// {/* <AboutUs /> */}
+//     </div>
+//   );
+// }
